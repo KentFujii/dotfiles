57b32f57b780ac554e2c0895da0b07e5017ba732
KentFujii <kent.where.the.light.is@gmail.com>
2020-03-12 19:08:59 +0900

    update: brew

--- brew.sh
+++ brew.sh
@@ -3,6 +3,8 @@ brew install tmux
 brew install reattach-to-user-namespace
 brew install cmatrix
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install ansible
+brew install terraform
 brew install mycli
 brew install pgcli
 brew install jq
@@ -10,3 +12,4 @@ brew install jid
 brew install peco
 brew install pt
 brew install csvkit
+brew install nkf
