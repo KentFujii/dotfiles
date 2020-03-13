c4ce9d4f6ba2b47ca097a275140d939541b28b56
KentFujii <kent.where.the.light.is@gmail.com>
2020-03-14 01:29:38 +0900

    add: brewbundle

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 ln -sf ~/work/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/work/dotfiles/.Brewfile ~/.Brewfile
 ln -sf ~/work/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/work/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/work/dotfiles/.ctags ~/.ctags
 ln -sf ~/work/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/work/dotfiles/.gitignore_global ~/.gitignore_global
+
+brew bundle --global
