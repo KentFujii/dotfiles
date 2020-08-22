c47bd958c9f136ac45428f7da2ec9d5e4846c7da
KentFujii <kent.where.the.light.is@gmail.com>
2020-08-22 16:07:56 +0900

    fix: brew

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd `dirname $0`
+cd $(dirname "$0") || exit
 
-# ln -sf ~/work/dotfiles/.bash_profile ~/.bash_profile
-# ln -sf ~/work/dotfiles/.Brewfile ~/.Brewfile
-# ln -sf ~/work/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/work/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/work/dotfiles/.Brewfile ~/.Brewfile
+ln -sf ~/work/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/work/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/work/dotfiles/.tmux.conf ~/.tmux.conf
-# ln -sf ~/work/dotfiles/.ctags ~/.ctags
-# ln -sf ~/work/dotfiles/.gitconfig ~/.gitconfig
-# ln -sf ~/work/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/work/dotfiles/.ctags ~/.ctags
+ln -sf ~/work/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/work/dotfiles/.gitignore_global ~/.gitignore_global
 
-# # Install package manager
+# Install package manager
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# # Install apps
-# brew bundle --global
-# # Install libraries
-# ansible-playbook playbook.yml
+# Install basic apps
+brew bundle --global
+# Provisioning
+ansible-playbook playbook.yml
