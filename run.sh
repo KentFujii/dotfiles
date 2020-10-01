159e3756c2adf6d4281788a3a3caafbb26498965
KentFujii <kent.where.the.light.is@gmail.com>
2020-10-01 23:28:47 +0900

    rm: nerdtree

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ ln -sf ~/work/dotfiles/.Brewfile ~/.Brewfile
 ln -sf ~/work/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/work/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/work/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/work/dotfiles/.ctags ~/.ctags
 ln -sf ~/work/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/work/dotfiles/.gitignore_global ~/.gitignore_global
 
