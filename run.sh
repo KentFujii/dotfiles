c8ccfb5ae340b1ce6e311f9b0f5323695babe2f3
KentFujii <kent.where.the.light.is@gmail.com>
2020-12-13 18:37:01 +0900

    simplify

--- run.sh
+++ run.sh
@@ -9,10 +9,3 @@ ln -sf ~/work/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/work/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/work/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/work/dotfiles/.gitignore_global ~/.gitignore_global
-
-# Install package manager
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# Install basic apps
-# brew bundle --global
-# Provisioning
-# ansible-playbook playbook.yml
